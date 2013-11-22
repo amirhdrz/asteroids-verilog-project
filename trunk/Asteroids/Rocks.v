c89bf45f2839a11615a8bd52aabb842bf106117d
amir.hdrz@gmail.com <amir.hdrz@gmail.com@45c9e10f-3d35-358a-6f24-c088514d9429>
2013-11-22 00:50:43 +0000

    Rocks_Man first draft

--- trunk/Asteroids/Rocks.v
+++ trunk/Asteroids/Rocks.v
@@ -23,20 +23,39 @@ input clk60hz,
 output pixel,
 
 // Output is high when this astroid is active
-output in_use);
+output reg inUse);
 
-/* initial_x and initial_y should be chosen randomly by a fibonnaci random number generator*/
+//initial_x and initial_y should be chosen randomly by a fibonnaci random number generator
 
+/* Registers */
 // Rock's position on the screen
 reg [9:0] rockX;
 reg [9:0] rockY;
 
-/* Movement generator */
 always @ (posedge clk60hz) begin
-	// TODO how to sign extend dirX and dirY, also, should they be in 2's complement?!
-	// TODO implement reset too
-	rockX <= rockX + dirX;
-	rockY <= rockY + dirY;
+	// Initialization
+	if ( start == 1'b1 ) begin
+		inUse <= 1'b1;
+		rockX <= initialX;
+		rockY <= initialY;
+	end else if ( reset == 1'b1 ) begin
+		inUse <= 1'b0;
+	end
+
+	// Movement
+	if ( inUse == 1'b1 ) begin
+		if (dirX[2] == 1'b0) begin
+				rockX <= rockX + dirX[1:0];
+		end else begin
+				rockX <= rockX - dirX[1:0];
+		end
+		
+		if (dirY[2] == 1'b0) begin
+			rockY <= rockY + dirY[1:0];
+		end else begin
+			rockY <= rockY - dirY[1:0];
+		end
+	end
 end
 
 
@@ -46,9 +65,8 @@ begin
 	if((px-12)<rockX && (px+12)>rockX && (py-10)<rockY&& (py+10)>rockY )
 		pixel=1'b1;
 	else if((px-10)<rockX && (px+10)>rockX && (py-12)<rockY&& (py+12)>rockY )
-		pixel=1'b1;
 	else pixel=1'b0;
-
+		pixel=1'b1;
 end
 
 endmodule
