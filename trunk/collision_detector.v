3fce8997d7f1ecbdea4169b70aaeb8b79d545aa8
KevinDang1993@gmail.com <KevinDang1993@gmail.com@45c9e10f-3d35-358a-6f24-c088514d9429>
2013-11-26 22:47:40 +0000

    'Kevin'

--- trunk/collision_detector.v
+++ trunk/collision_detector.v
@@ -1,4 +1,22 @@
 module collision_detector(
-);
+	input clk_60hz,
+	input [14:0]pixel,
+	output [14:0]reset);
+	
+	
+	reg lives;
+	reg score;
+	
+	always @ (posedge clk) begin
+		if(|pixel[0] and |pixel[14:5]) begin
+			reset = pixel;
+		end
+		else if (|pixel[4:1] and |pixel[14:5]) begin
+			reset = pixel;
+		end
+		else begin
+			reset = 14'b0;
+		end
+	end
 
-endmodule
+end module
